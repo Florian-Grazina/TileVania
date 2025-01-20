84cc6381d0da44d93ba4b6fbe2d40479fa81894e
Florian Grazina <Floriangrazina@gmail.com>
2025-01-20 22:52:41 +0100

    create smooth flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveInput;
     private Rigidbody2D redRigidbody;
+    private Coroutine flipCoroutine;
 
     protected void Start()
     {
@@ -16,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     protected void Update()
     {
         Run();
+        FlipSprite();
     }
 
     #region input system methods
@@ -31,6 +35,33 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new (moveInput.x * moveSpeed, redRigidbody.linearVelocityY);
         redRigidbody.linearVelocity = playerVelocity;
+    }
+
+    protected void FlipSprite()
+    {
+        if(flipCoroutine != null)
+            StopCoroutine(flipCoroutine);
+
+        float xScale = Mathf.Sign(redRigidbody.linearVelocityX);
+        flipCoroutine = StartCoroutine(SmoothFlip(xScale));
+    }
+
+    private IEnumerator SmoothFlip(float xScale)
+    {
+        Vector2 currentScale = transform.localScale;
+        Vector2 targetScale = new (xScale, currentScale.y);
+
+        float progress = 0f;
+        float duration = 1f;
+
+        while (progress < duration)
+        {
+            progress += Time.deltaTime * 10f;
+            transform.localScale = Vector2.Lerp(currentScale, targetScale, progress);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
     }
     #endregion
 }
