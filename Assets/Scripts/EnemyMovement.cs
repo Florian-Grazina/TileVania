2a0c52c880a118ec615c5c8b7bb242fe202c40c1
Florian Grazina <Floriangrazina@gmail.com>
2025-02-08 14:39:04 +0100

    flip enemies

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] protected float moveSpeed = 1f;
+    [SerializeField] protected float moveSpeed = -1f;
     Rigidbody2D myRigidbody;
+
+    private Coroutine flipCoroutine;
 
     protected void Start()
     {
@@ -17,12 +21,37 @@ public class EnemyMovement : MonoBehaviour
 
     protected void OnTriggerExit2D(Collider2D collision)
     {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+            return;
+
         moveSpeed *= -1;
+        FlipSprite();
     }
 
-    private void FlipEnemyFacing()
+    protected void FlipSprite()
     {
-        transform.localScale = new Vector2(myRigidbody.linearVelocity.x * -1, 1);
+        if (flipCoroutine != null)
+            StopCoroutine(flipCoroutine);
 
+        float xScale = Mathf.Sign(myRigidbody.linearVelocityX);
+        flipCoroutine = StartCoroutine(SmoothFlip(xScale));
+    }
+
+    private IEnumerator SmoothFlip(float xScale)
+    {
+        Vector2 currentScale = transform.localScale;
+        Vector2 targetScale = new(xScale, currentScale.y);
+
+        float progress = 0f;
+        float duration = 1f;
+
+        while (progress < duration)
+        {
+            progress += Time.deltaTime * 10f;
+            transform.localScale = Vector2.Lerp(currentScale, targetScale, progress);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
     }
 }
