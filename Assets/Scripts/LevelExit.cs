f311a6bac5771de9fd0996b9e3fb5a511de3b7eb
Florian Grazina <Floriangrazina@gmail.com>
2025-02-19 23:23:53 +0100

    add coroutine for exit level

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,9 +1,22 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private float levelLoadDelay = 2f;
+
+    protected void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
